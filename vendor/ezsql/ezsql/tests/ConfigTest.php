d8174ac3d13b484551d3bf9c40dea0e22d9aebd1
admin <vagabond_ac@hotmail.com>
2020-04-17 18:41:54 +0700

    test

---
+++ vendor/ezsql/ezsql/tests/ConfigTest.php
@@ -0,0 +1,265 @@
+<?php
+
+namespace ezsql\Tests;
+
+use ezsql\Config;
+use ezsql\ConfigAbstract;
+use ezsql\ConfigInterface;
+use ezsql\Tests\EZTestCase;
+
+class ConfigTest extends EZTestCase 
+{
+    /**
+    * @covers ezsql\Config::setupMysqli
+    */
+    public function testSetupMysqli()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped(
+              'The MySQLi extension is not available.'
+            );
+        }
+
+        $settings = new Config('mysqli', [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $this->assertTrue($settings instanceof ConfigInterface);
+        $this->assertEquals(self::TEST_DB_USER, $settings->getUser());
+        $this->assertEquals(self::TEST_DB_PASSWORD, $settings->getPassword());
+        $this->assertEquals(self::TEST_DB_NAME, $settings->getName());
+    }
+
+    public function testInitializeMysqli()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped(
+              'The MySQLi extension is not available.'
+            );
+        }
+
+        $settings = Config::initialize('mysqli', [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $this->assertTrue($settings instanceof ConfigInterface);
+    }
+
+    public function testErrorMysqli()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped(
+              'The MySQLi extension is not available.'
+            );
+        }
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageRegExp('/[Missing configuration details to connect to database]/');
+        $settings = Config::initialize('mysqli', [self::TEST_DB_USER, self::TEST_DB_PASSWORD]);
+    }
+
+    /**
+    * @covers ezsql\Config::setupPdo
+    */
+    public function testSetupPdo()
+    {
+        if ( ! \class_exists ('PDO') ) {
+            $this->markTestSkipped(
+              'The PDO Lib is not available.'
+            );
+        }
+
+        $dsn = 'mysql:host='.self::TEST_DB_HOST.';dbname='. self::TEST_DB_NAME.';port=3306';
+        $settings = new Config('pdo', [$dsn, self::TEST_DB_USER, self::TEST_DB_PASSWORD]);
+        $this->assertTrue($settings instanceof ConfigAbstract);
+    }
+
+    public function testInitializePdo()
+    {
+        if ( ! \class_exists ('PDO') ) {
+            $this->markTestSkipped(
+              'The PDO Lib is not available.'
+            );
+        }
+
+        $dsn = 'mysql:host='.self::TEST_DB_HOST.';dbname='. self::TEST_DB_NAME.';port=3306';
+        $settings = Config::initialize('pdo', [$dsn, self::TEST_DB_USER, self::TEST_DB_PASSWORD]);
+        $this->assertTrue($settings instanceof ConfigAbstract);
+        $this->assertEquals($dsn, $settings->getDsn());
+        $this->assertEquals(self::TEST_DB_USER, $settings->getUser());
+        $this->assertEquals(self::TEST_DB_PASSWORD, $settings->getPassword());
+    }
+
+    public function testErrorPdo()
+    {
+        if ( ! \class_exists ('PDO') ) {
+            $this->markTestSkipped(
+              'The PDO Lib is not available.'
+            );
+        }
+
+        $dsn = 'mysql:host='.self::TEST_DB_HOST.';dbname='. self::TEST_DB_NAME.';port=3306';
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageRegExp('/[Missing configuration details to connect to database]/');
+        $settings = Config::initialize('pdo', [$dsn]);
+    }
+
+    public function test__callPdo()
+    {
+        if ( ! \class_exists ('PDO') ) {
+            $this->markTestSkipped(
+              'The PDO Lib is not available.'
+            );
+        }
+
+        $dsn = 'mysql:host='.self::TEST_DB_HOST.';dbname='. self::TEST_DB_NAME.';port=3306';
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageRegExp('/[does not exist]/');
+        $settings = new Config('pdo', [$dsn, self::TEST_DB_USER, self::TEST_DB_PASSWORD]);
+        $settings->getNotAnProperty();
+    }
+
+    /**
+    * @covers ezsql\Config::setupPgsql
+    */
+    public function testSetupPgsql()
+    {
+        if (!extension_loaded('pgsql')) {
+            $this->markTestSkipped(
+              'The PostgreSQL Lib is not available.'
+            );
+        }
+
+        $settings = new Config('pgsql', [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME, self::TEST_DB_HOST, self::TEST_DB_PORT]);
+        $this->assertTrue($settings instanceof ConfigInterface);
+        $this->assertEquals(self::TEST_DB_USER, $settings->getUser());
+        $this->assertEquals(self::TEST_DB_PASSWORD, $settings->getPassword());
+        $this->assertEquals(self::TEST_DB_NAME, $settings->getName());
+        $this->assertEquals(self::TEST_DB_HOST, $settings->getHost());
+        $this->assertEquals(self::TEST_DB_PORT, $settings->getPort());
+    }
+
+    public function testInitializePgsql()
+    {
+        if (!extension_loaded('pgsql')) {
+            $this->markTestSkipped(
+              'The PostgreSQL Lib is not available.'
+            );
+        }
+
+        $settings = Config::initialize('pgsql', [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME, self::TEST_DB_HOST, self::TEST_DB_PORT]);
+        $this->assertTrue($settings instanceof ConfigInterface);
+    }
+
+    public function testErrorPgsql()
+    {
+        if (!extension_loaded('pgsql')) {
+            $this->markTestSkipped(
+              'The PostgreSQL Lib is not available.'
+            );
+        }
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageRegExp('/[Missing configuration details to connect to database]/');
+        $settings = Config::initialize('pgsql', [self::TEST_DB_USER, self::TEST_DB_PASSWORD]);
+    }
+
+    /**
+    * @covers ezsql\Config::setupSqlsrv
+    */
+    public function testSetupSqlsrv()
+    {
+        if (!extension_loaded('sqlsrv')) {
+            $this->markTestSkipped(
+              'The sqlsrv Lib is not available.'
+            );
+        }
+
+        $settings = new Config('sqlsrv', [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $this->assertTrue($settings instanceof ConfigAbstract);
+        $this->assertEquals(self::TEST_DB_USER, $settings->getUser());
+        $this->assertEquals(self::TEST_DB_PASSWORD, $settings->getPassword());
+        $this->assertEquals(self::TEST_DB_NAME, $settings->getName());
+    }
+
+    public function testInitializeSqlsrv()
+    {
+        if (!extension_loaded('sqlsrv')) {
+            $this->markTestSkipped(
+              'The sqlsrv Lib is not available.'
+            );
+        }
+
+        $settings = Config::initialize('sqlsrv', [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $this->assertTrue($settings instanceof ConfigAbstract);
+    }
+
+    public function testErrorSqlsrv()
+    {
+        if (!extension_loaded('sqlsrv')) {
+            $this->markTestSkipped(
+              'The sqlsrv Lib is not available.'
+            );
+        }
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageRegExp('/[Missing configuration details to connect to database]/');
+        $settings = new Config('sqlsrv', [self::TEST_DB_USER, self::TEST_DB_PASSWORD]);
+    }
+
+    /**
+    * @covers ezsql\Config::setupSqlite3
+    */
+    public function testSetupSqlite3()
+    {
+        if (!extension_loaded('sqlite3')) {
+            $this->markTestSkipped(
+              'The sqlite3 Lib is not available.'
+            );
+        }
+        
+        $settings = new Config('sqlite3', [self::TEST_SQLITE_DB_DIR, self::TEST_SQLITE_DB]);
+        $this->assertTrue($settings instanceof ConfigInterface);
+        $this->assertEquals(self::TEST_SQLITE_DB_DIR, $settings->getPath());
+        $this->assertEquals(self::TEST_SQLITE_DB, $settings->getName());
+    }
+    
+    public function testInitializeSqlite3()
+    {
+        if (!extension_loaded('sqlite3')) {
+            $this->markTestSkipped(
+              'The sqlite3 Lib is not available.'
+            );
+        }
+        
+        $settings = Config::initialize('sqlite3', [self::TEST_SQLITE_DB_DIR, self::TEST_SQLITE_DB]);
+        $this->assertTrue($settings instanceof ConfigInterface);
+    }
+
+    public function testErrorSqlite3()
+    {
+        if (!extension_loaded('sqlite3')) {
+            $this->markTestSkipped(
+              'The sqlite3 Lib is not available.'
+            );
+        }
+        
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageRegExp('/[Missing configuration details to connect to database]/');
+        $settings = new Config('sqlite3', [self::TEST_SQLITE_DB_DIR]);
+    }
+
+    /**
+    * @covers ezsql\Config::__construct
+    */
+    public function test_construct()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageRegExp('/[Missing configuration details to connect to database]/');
+        $settings = new Config('', [self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+    }
+
+    /**
+    * @covers ezsql\Config::__construct
+    */
+    public function test_constructArgs()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageRegExp('/[Missing configuration details to connect to database]/');
+        $settings = new Config('mysqli');
+    }
+} 
---
+++ vendor/ezsql/ezsql/tests/ezSchemaTest.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace ezsql\Tests;
+
+use ezsql\ezSchema;
+use ezsql\Tests\EZTestCase;
+
+class ezSchemaTest extends EZTestCase 
+{		
+    /**
+    * @covers ezsql\ezSchema::vendor
+    */
+    public function testVendor()
+    {
+        clearInstance();
+        $this->assertEquals(null, getVendor());
+        $this->assertEquals(false, ezSchema::datatype(BLOB, NULLS));
+        $this->assertFalse(column('id', INTR, 32, AUTO, PRIMARY));
+    }
+
+    /**
+    * @covers ezsql\ezSchema::vendor
+    */
+    public function testVendor_mysqli()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped(
+              'The MySQLi extension is not available.'
+            );
+        }
+
+        mysqlInstance([self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $this->assertEquals(MYSQLI, getVendor());
+        $this->assertEquals('BLOB NULL', ezSchema::datatype(BLOB, NULLS));
+        $this->assertEquals('VARCHAR(256) NOT NULL', ezSchema::datatype(VARCHAR, 256, notNULL));
+        $this->assertEquals('id INT(32) AUTO_INCREMENT PRIMARY KEY, ', column('id', INTR, 32, AUTO, PRIMARY));
+    }
+
+    /**
+    * @covers ezsql\ezSchema::datatype
+    */
+    public function testDatatype_mysqli()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped(
+              'The MySQLi extension is not available.'
+            );
+        }
+
+        $db = mysqlInstance([self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $result = $db->create('profile',
+            'id '. ezSchema::datatype(INTR, 11, PRIMARY). ', ',
+            'name '.ezSchema::datatype(VARCHAR, 256, notNULL). ', '
+        );
+
+        $this->assertEquals(0, $result);
+        $db->drop('profile');
+    }
+
+    /**
+    * @covers ezsql\ezSchema::column
+    */
+    public function testColumn()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped(
+              'The MySQLi extension is not available.'
+            );
+        }
+
+        $db = mysqlInstance([self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $result = $db->create('profile',
+            column('id', INTR, 32, AUTO),
+            column('name', CHAR, 32, notNULL),
+            primary('id_pk', 'id'),
+            index('name_dx', 'name')
+        );
+
+        $this->assertEquals(0, $result);
+        $db->drop('profile');
+    }
+
+    /**
+    * @covers ezsql\ezSchema::__call
+    */
+    public function test__call()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped(
+              'The MySQLi extension is not available.'
+            );
+        }
+
+        $db = mysqlInstance([self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $result = $db->create('profile',
+            column('id', INTR, 32, AUTO, PRIMARY),
+            column('name', VARCHAR, 256, notNULL), 
+            column('price', NUMERIC, 6,2), 
+            column('date', TIMESTAMP, notNULL), 
+            column('pics', BLOB, NULLS)
+        );
+
+        $this->assertEquals(0, $result);
+        $db->drop('profile');
+    }
+
+    /**
+    * @covers ezsql\ezSchema::__call
+    */
+    public function test__call_Error()
+    {
+        if (!extension_loaded('mysqli')) {
+            $this->markTestSkipped(
+              'The MySQLi extension is not available.'
+            );
+        }
+
+        clearInstance();
+        $this->assertFalse(column('id', INTR, 32, AUTO, PRIMARY));
+        $db = mysqlInstance([self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessageRegExp('/[does not exist]/');
+        $this->assertNull(column('id', 'DOS', 32));
+    }
+
+    /**
+    * @covers ezsql\ezSchema::vendor
+    */
+    public function testVendor_Pgsql()
+    {
+        if (!extension_loaded('pgsql')) {
+            $this->markTestSkipped(
+              'The PostgreSQL Lib is not available.'
+            );
+        }
+
+        pgsqlInstance([self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME, self::TEST_DB_HOST, self::TEST_DB_PORT]);
+        $this->assertEquals(PGSQL, getVendor());
+        $this->assertEquals('TIMESTAMP NOT NULL', ezSchema::datatype(TIMESTAMP, notNULL));
+        $this->assertEquals('price NUMERIC(6,2) NULL, ', column('price', NUMERIC, 6, 2, NULLS));
+        $this->assertEquals('id SERIAL PRIMARY KEY, ', column('id', AUTO, PRIMARY));
+    }
+
+    /**
+    * @covers ezsql\ezSchema::vendor
+    */
+    public function testVendor_Sqlite3()
+    {
+        if (!extension_loaded('sqlite3')) {
+            $this->markTestSkipped(
+              'The sqlite3 Lib is not available.'
+            );
+        }
+        
+        sqliteInstance([self::TEST_SQLITE_DB_DIR, self::TEST_SQLITE_DB]);
+        $this->assertEquals(SQLITE3, getVendor());
+    }
+
+    /**
+    * @covers ezsql\ezSchema::vendor
+    */
+    public function testVendor_Sqlsrv()
+    {
+        if (!extension_loaded('sqlsrv')) {
+            $this->markTestSkipped(
+              'The sqlsrv Lib is not available.'
+            );
+        }
+
+        mssqlInstance([self::TEST_DB_USER, self::TEST_DB_PASSWORD, self::TEST_DB_NAME]);
+        $this->assertEquals(MSSQL, getVendor());
+    }
+
+    /**
+    * @covers ezsql\ezSchema::vendor
+    */
+    public function testVendor_Pdo()
+    {
+        if ( ! \class_exists ('PDO') ) {
+            $this->markTestSkipped(
+              'The PDO Lib is not available.'
+            );
+        }
+
+        $pdo_mysql = pdoInstance(['mysql:host='.self::TEST_DB_HOST.';dbname='.self::TEST_DB_NAME.';port=3306', self::TEST_DB_USER,self::TEST_DB_PASSWORD]);
+        $pdo_mysql->connect();
+        $this->assertEquals(MYSQLI, getVendor());
+    }
+
+    /**
+    * @covers ezsql\ezSchema::__construct
+    */
+    public function test__construct()
+    {
+        $this->assertNotNull(new ezSchema('test'));
+    }
+}
